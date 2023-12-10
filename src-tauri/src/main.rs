6f043f1363571fd0e19f04bb27465f4fbdacb4e9
Jonathan Bangert <jonathan@bangert.dk>
2023-12-10 10:12:33 +0100

    Fix builds and stuff

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Manager};
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
+
 fn main() {
     #[cfg(debug_assertions)] // only enable instrumentation in development builds
     let devtools = devtools::init();
@@ -11,6 +19,59 @@ fn main() {
     let builder = builder.plugin(devtools);
     // Run the tauri application
     builder
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+
+            app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+        }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
+        })
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window: tauri::Window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            }
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window: tauri::Window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    app.exit(1);
+                }
+                "hide" => {
+                    let window: tauri::Window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                "show" => {
+                    let window: tauri::Window = app.get_window("main").unwrap();
+                    window.show().unwrap();
+                }
+                "relaunch" => {
+                    tauri::api::process::restart(&app.env());
+                }
+                "update" => {
+                    app.updater();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .system_tray(tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
